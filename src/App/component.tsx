95160efb8b4388f7a6cbba1a1affd9372e593914
Víctor Falcón <victoor89@gmail.com>
2026-02-24 14:09:58 +0100

    fix: build issues

--- src/App/component.tsx
+++ src/App/component.tsx
@@ -3,20 +3,18 @@ import { type FC, useState } from 'react'
 import BookmarksComponent from '../Bookmarks/component'
 import NetworkStatusComponent from '../NetworkStatus/component'
 import PreferencesComponent from '../Preferences/component'
-import SearchComponent from '../Search/component'
 import { useToggles } from '../Toggles/repository'
 import TopSitesComponent from '../TopSites/component'
 
 const App: FC = () => {
   const [toggles] = useToggles()
-  const [search, setSearch] = useState<string>('')
+  const [search, _] = useState<string>('')
   const [openPreferences, setOpenPreferences] = useState(false)
   return (
     <div className="App">
       {toggles?.topSites ? (
         <div className="App__Header">
           <TopSitesComponent search={search} />
-          {/* <SearchComponent value={search} onChange={setSearch} /> */}
         </div>
       ) : null}
       {toggles?.bookmarks ? <BookmarksComponent search={search} /> : null}
